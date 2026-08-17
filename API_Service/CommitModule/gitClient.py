a2eac03b8b29c6570707f16d6baaca3cf788f786
PangoAguirre <bcamilo.aguirre@udea.edu.co>
2026-08-16 23:24:45 -0500

    fix: arreglar cómo se obtiene el nombre después de la factorización

--- API_Service/CommitModule/gitClient.py
+++ API_Service/CommitModule/gitClient.py
@@ -7,7 +7,7 @@ class gitClient:
         self.emailUser = emailUser
 
     def clone_repository(self, repository_url, destination_directory):
-        name = self.get_nameRepo(repository_url)
+        name = repository_url.split("/")[-1].replace(".git", "")
         subprocess.run(["git", "config", "--global", "user.name", self.nameUser])
         subprocess.run(["git", "config", "--global", "user.email", self.emailUser])
         subprocess.run(["mkdir", "-p", destination_directory + "/" + name])
