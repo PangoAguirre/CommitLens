bd42d710323b6164b1befbfa8d893d6412703b18
PangoAguirre <bcamilo.aguirre@udea.edu.co>
2026-08-16 23:24:19 -0500

    fix: corregir como se creaba y escribia el archivo con los commits

--- API_Service/CommitModule/commitExtractor.py
+++ API_Service/CommitModule/commitExtractor.py
@@ -6,8 +6,8 @@ class gitCommitExtractor:
         pass
 
     def extract_allcommits(self, name, destination_directory):
-        repo_path = f"{destination_directory}/{name}"
-        subprocess.run(["cd", repo_path])
-        subprocess.run(["git", "log", "--all", "--pretty=format:'COMMIT|%H|%an|%ae|%aI|%s|%P'", 
-                        "--numstat", ">", f"{destination_directory}/commits_{name}.txt"])
+        with open(f"{destination_directory}/commits_{name}.txt", "w") as file:
+            subprocess.run(["git", "log", "--all", "--pretty=format:'COMMIT|%H|%an|%ae|%aI|%s|%P'", 
+                        "--numstat"], stdout=file)
+        
         
