5a198fcb622d8e3f9458dd30e2acfe6c52f7e515
PangoAguirre <bcamilo.aguirre@udea.edu.co>
2026-08-16 23:26:13 -0500

    refactor: organizar nombres de variables e importaciones

--- API_Service/CommitModule/gitController.py
+++ API_Service/CommitModule/gitController.py
@@ -1,18 +1,17 @@
-from API_Service.CommitModule import Repo, gitClient, commitExtractor
+import Repo, Client, commitExtractor
 
 class gitController:
     def __init__(self, nameUser, emailUser):
-        self.destination_directory = "./API_Service/CloneRepository"
-        self.git_client = gitClient(nameUser, emailUser)
-        self.gitRepoInfo = Repo.gitRepo()
+        self.destination_directory = "API_Service/CloneRepository"
+        self.client = Client.gitClient(nameUser, emailUser)
+        self.repo = Repo.gitRepo()
         self.commitExtractor = commitExtractor.gitCommitExtractor()
 
     def clone_repository(self, repository_url):
-        self.git_client.clone_repository(repository_url, self.destination_directory)
+        self.client.clone_repository(repository_url, self.destination_directory)
 
     def get_infoRepo(self, repository_url):
-        return self.gitRepoInfo.get_infoRepo(repository_url, self.destination_directory)
+        return self.repo.get_info(repository_url, self.destination_directory)
     
     def extract_allcommits(self, name):
         self.commitExtractor.extract_allcommits(name, self.destination_directory)
-    
